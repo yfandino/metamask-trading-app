04fef09427287414a2b6c49a4abea51734e2f030
Yanniel Fandiño <yfandino@aryaly.com>
2021-08-28 00:56:10 +0200

    Update useMetamask hook to return web3 instance

--- frontend/src/lib/useMetamask.js
+++ frontend/src/lib/useMetamask.js
@@ -1,18 +1,25 @@
 import { useEffect, useRef, useState } from "react";
 import Metamask from "@metamask/onboarding";
+import Web3 from "web3";
 
 export default function useMetamask() {
   const [isInstalled, setIsInstalled] = useState();
   const onboarding = useRef();
+  const web3 = useRef();
 
   useEffect(function initialize() {
     onboarding.current = new Metamask();
     setIsInstalled(Metamask.isMetaMaskInstalled());
   }, []);
 
+  useEffect(function initializeWeb3() {
+    if (!isInstalled) return;
+    web3.current = new Web3(window.ethereum);
+  }, [isInstalled]);
 
   return {
     isMetamaskInstalled: isInstalled,
-    onboarding: onboarding.current
+    onboarding: onboarding.current,
+    web3: web3.current
   };
 }
