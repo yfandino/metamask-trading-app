f0bd8acb672385edd611bb9448353b651ec30114
Yanniel Fandiño <yfandino@aryaly.com>
2021-08-28 16:05:09 +0200

    Fix accepted headers

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,13 @@ app.use(express.json());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept," +
+                                             " Authorization, x-auth-signature, x-auth-wallet");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   next();
 });
 
