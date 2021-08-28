f3aa5ba5af717d0f3978973f2ad51a5ca601e440
Yanniel Fandiño <yfandino@aryaly.com>
2021-08-28 14:38:59 +0200

    Add auth util function

---
+++ backend/src/controllers/utils/auth.js
@@ -0,0 +1,23 @@
+const ethUtil = require("ethereumjs-util");
+const SECRET = 'TEST_SECRET';
+
+module.exports.checkSignature = async (req, res, next) => {
+  const signature = req.headers["x-auth-signature"];
+  const wallet = req.headers["x-auth-wallet"];
+
+  let nonce = "\x19Ethereum Signed Message:\n" + SECRET.length + SECRET;
+
+  nonce = ethUtil.keccak(Buffer.from(nonce, "utf-8"));
+  const { v, r, s } = ethUtil.fromRpcSig(signature);
+  const publicKey = ethUtil.ecrecover(ethUtil.toBuffer(nonce), v, r, s);
+  const addressBuffer = ethUtil.pubToAddress(publicKey);
+  const address = ethUtil.bufferToHex(addressBuffer);
+
+  if (address !== wallet) {
+    const error = new Error("Unauthorized");
+    error.status = 401;
+    return next(error);
+  }
+
+  next();
+}
